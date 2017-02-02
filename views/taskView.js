a38f5901516b172e3486a198e4c3dbce2d1b9f8f
Tomasz Stelagowski <Tomasz Stelagowski>
2017-02-02 16:00:17 +0100

    bugFix

--- views/taskView.js
+++ views/taskView.js
@@ -15,7 +15,7 @@ define([
 					this.model.set("DONE", "Y");
 				}
 				//this.model.save();
-				this.model.destroy(null, {data: {page: 3}});
+				this.model.save(null, {data: {page: 3}});
 			},
 			template: _.template(tmpl, {}),
 			initialize: function(){

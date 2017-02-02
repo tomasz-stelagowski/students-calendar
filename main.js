76a9d0b2c31e331012afb0988961bc238199af5d
Tomasz Stelagowski <Tomasz Stelagowski>
2017-02-02 21:29:28 +0100

    bugFix

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ define([
 	], function($, _, Backbone, MainView){
 	var SessionView = Backbone.View.extend({
 		initialize: function(){
-			var mainView = new MainView({el: $("body")});
+			var mainView = new MainView({el: $("#app")});
 
 		},
 		render: function(){

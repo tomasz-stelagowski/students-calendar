abffa358ee5025e34a4bc5f98fc263a3c48644be
Tomasz Stelagowski <Tomasz Stelagowski>
2017-02-02 17:16:35 +0100

    bugFix

--- api/tasks.php
+++ api/tasks.php
@@ -6,11 +6,12 @@ $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
  
-// connect to the mysql database
+// connect to the oracle database
 $pass = file_get_contents('../../../password.txt');
 $link = oci_connect('ts340234', $pass, '');
  
 // retrieve the table and key from the path
+echo $request;
 $table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
 $key = array_shift($request)+0;
 
@@ -24,6 +25,8 @@ switch ($method) {
     $sql = "select * from view_get_task_list where day = TO_DATE(" . "'" . $date . "', 'YYYY-MM-DD')"; 
     break;
   case 'POST':
+    $over_method = $_POST[''];
+
     $sql = "update to_do_items set done = 'Y' where id=" . $key; 
     break;
   case 'DELETE':

ba02d018f73567fce3a7c5b2e8f6cf414d83c75a
Tomasz Stelagowski <Tomasz Stelagowski>
2017-02-02 10:41:29 +0100

    Bugfix

--- views/tasksDisplayView.js
+++ views/tasksDisplayView.js
@@ -44,7 +44,7 @@ define([
 			render: function(){
 				this.$el.html("");
 				_.each(this.tasksViews, function(task){
-					this.$el.append(task.render().el.innerHTML);
+					this.$el.append($(task.render().el.html())[0]);
 				}, this);	
 			},
 			remove: function(){
